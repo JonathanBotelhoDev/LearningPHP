a414c54db3c6a8ce3dfb88aeb28a6bae60f1487f
JonathanBotelhoDev <jonathanviniciusbotelho@gmail.com>
2024-07-30 10:39:11 -0300

    até os 30 minutos

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
  Aprendendo PHP do completo 0
 
  Primeiro vídeo utilizado: https://www.youtube.com/watch?v=mRogW0vMJuI&list=PLoVcb2fggseov9AGUKbFoirNo5SQeSskG&index=3
+
+ links bons de se ler: https://www.php.net/manual/pt_BR/reserved.constants.php
--- Video4H/Public/index.php
+++ Video4H/Public/index.php
@@ -1,7 +1,78 @@
 <?php
 
+echo 'Parte 1: Include e require';
+
+// include './teste.php';
+// include_once './teste.php';
+// require_once './teste.php';
 require './teste.php';
 
-echo $name;
+
+echo "<br>$name";
+echo '<br>';
+
+echo '<br>Parte 2: tipos de variáveis<br>';
+
+// string
+echo gettype('blá blá blá');
+echo '<br><br>';
+
+// numbers
+echo gettype(10);
+echo '<br>';
+echo gettype(10.0);
+echo '<br><br>';
+
+// booleans
+echo gettype(true);
+echo gettype(false);
+echo '<br><br>';
+
+// arrays
+echo gettype(['blá blá blá', 10, 10.0, true, false]);
+echo '<br><br>';
+
+// object
+class Person{
+    public function teste(){
+        # code...
+    }
+}
+
+echo gettype(new Person);
+echo '<br><br>';
+
+
+// null
+echo gettype(null);
+echo '<br><br>';
+
+$idade = 19;
+$racahumana; // Variável sem um valor atribuido
+
+echo "$idade é do tipo: ";
+echo gettype($idade);
+
+/*  Trabalhando com nomes compostos
+Existem dois padrões para nomear uma variável com nome composto: camelCase e snake_case */
+
+// camelCase: A primeira letra das outras palavras sempre será maiúscula
+$myName;
+
+// snake_case: separa cada palavra diferente com underline(_)
+$my_name;
+
+// Atribuindo uma variável como valor para outro variável
+$nome1 = "Lorrany"; // Criou-se uma variável
+$nome2 = $nome1; // Criou-se outra variável e deu o valor da primeira variável para ela
+//$nome2 = &$nome1; // Se colocado o "&" antes a variável usado como valor, signifca que está passando como um referencia, ou seja, as variáveis em questão irão apontar para o mesmo ponto na memória. Quando se altera um, o outro também será alterado, já que "compartilham" o mesmo espaço na memoria.
+$nome1 = "Joninhas"; // Mudou SOMENTE o valor da primeira variável
+
+echo "<br><br>Variável 1 = $nome1<br>";
+echo "Variável 2 = $nome2";
+
+echo '<br><br>Parte 3: Constantes<br>';
+
+define();
 
 ?>
